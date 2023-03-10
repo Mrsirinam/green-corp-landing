8103f8d1adacb6bc2b493154854575fff47f22c7
Mrsirinam <myworkmail2016@mail.ru>
2023-03-10 20:38:34 +0300

    добавила поле в форму

--- js/animations.js
+++ js/animations.js
@@ -21,3 +21,26 @@ function initIncreaseNumberAnimation () {
 	increaseNumberAnimationStep(0, element, 5000)
 }
 initIncreaseNumberAnimation ()
+
+document.querySelector('#budget') .addEventListener('change', function 
+handleSelectChange(event) {
+  if (event.target.value === 'other') {
+    // Должны добавить еще одно текстовое поле
+		const formContainer = document.createElement('div');
+    formContainer.classList.add('form__group');
+    formContainer.classList.add('form__other-input');
+ 
+    const input = document.createElement('input');
+    input.placeholder = "Введите ваш вариант";
+    input.type = "text";
+ 
+    formContainer.appendChild(input);
+    document.querySelector('#form form').insertBefore(formContainer, document.querySelector('.form__submit')); 
+  }
+	const otherInput = document.querySelector('.form__other-input');
+  if (event.target.value !== 'other' && otherInput) {
+    // Удаляем ранее добавленное текстовое поле, если оно есть в DOM
+		const otherInput = document.querySelector('.form__other-input');
+    document.querySelector('#form form').removeChild(otherInput);
+	}
+});
